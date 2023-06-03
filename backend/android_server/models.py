2074a2ec0f604991a5c3e9c78c2fbfafd4164f0b
ChimpOnCloud <sunfy20@mails.tsinghua.edu.cn>
2023-06-03 22:29:10 +0800

    merging

--- backend/android_server/models.py
+++ backend/android_server/models.py
@@ -18,32 +18,33 @@ class account(models.Model):
     introduction = models.CharField(max_length=50, verbose_name='简介')
 
 
-# class image(models.Model):
-#     image_content = models.IntegerField(primary_key=True)
+class image(models.Model):
+    image_content = models.IntegerField(primary_key=True)
 
 
-# class pyq(models.Model):
-#     avatar = models.IntegerField(verbose_name='头像')
-#     tag = models.CharField(verbose_name='tag', max_length=25)
-#     username = models.CharField(max_length=25, verbose_name='用户名')
-#     ID = models.AutoField(primary_key=True)
-#     posttime = models.DateTimeField(auto_now_add=True, verbose_name='发表时间')
-#     title = models.CharField(max_length=25, verbose_name='标题')
-#     content = models.CharField(max_length=500, verbose_name='内容')
-#     image_contain = models.ManyToManyField(image)
+class pyq(models.Model):
+    avatar = models.IntegerField(verbose_name='头像')
+    tag = models.CharField(verbose_name='tag', max_length=25)
+    username = models.CharField(max_length=25, verbose_name='用户名')
+    ID = models.AutoField(primary_key=True)
+    posttime = models.DateTimeField(auto_now_add=True, verbose_name='发表时间')
+    title = models.CharField(max_length=25, verbose_name='标题')
+    content = models.CharField(max_length=500, verbose_name='内容')
+    image_contain = models.ManyToManyField(image)
 
 
-# class message(models.Model):
-#     msg_content = models.CharField(max_length=500, verbose_name='消息内容')
-#     msg_ID = models.IntegerField(verbose_name='消息序号')
-#     msg_time = models.DateTimeField(auto_now_add=True, verbose_name='发表时间')
+class message(models.Model):
+    msg_content = models.CharField(max_length=500, verbose_name='消息内容')
+    msg_ID = models.IntegerField(verbose_name='消息序号')
+    msg_time = models.DateTimeField(auto_now_add=True, verbose_name='发表时间')
 
 
-# class chat(models.Model):
-#     msg_contain = models.ManyToManyField(message)
-#     oppo_id = models.AutoField(verbose_name='对面ID', primary_key=True)
-#     msg_cnt = models.IntegerField(verbose_name='消息数量')
+class chat(models.Model):
+    msg_contain = models.ManyToManyField(message)
+    oppo_id = models.AutoField(verbose_name='对面ID', primary_key=True)
+    msg_cnt = models.IntegerField(verbose_name='消息数量')
 
 
-# class chats(models.Model):
-#     chat_contain = models.ManyToManyField(chat)
+class chats(models.Model):
+    user_ID = models.AutoField(primary_key=True)
+    chat_contain = models.ManyToManyField(chat)
